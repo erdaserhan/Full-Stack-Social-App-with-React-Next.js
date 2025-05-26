c6b2aacd6f9b2880e988196316d03ec4ee32db41
erdaserhan <erhan209@gmail.com>
2025-05-26 22:27:58 +0200

    npm install next-themes fot the dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
+        <SignOutButton>
+          <Button>
+           SignOut 
+          </Button>  
+        </SignOutButton> 
       </SignedIn>
     </div>
   );
