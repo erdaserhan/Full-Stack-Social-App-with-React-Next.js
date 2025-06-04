484089da0a24ca70a811276cc3b9c00ee1dd49ae
erdaserhan <erhan209@gmail.com>
2025-06-04 15:52:29 +0200

    getUserByClerkId function created to get the current user details from DB

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,49 @@
-function Sidebar() {
+import { currentUser } from "@clerk/nextjs/server"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
+import { SignInButton, SignUpButton } from "@clerk/nextjs"
+import { Button } from "./ui/button"
+import { getUserByClerkId } from "@/actions/user.action"
+import { use } from "react"
+
+async function Sidebar() {
+  const authUser = await currentUser()
+  if(!authUser) return <UnAuthenticatedSidebar />
+
+  const user = await getUserByClerkId(authUser.id)
+  if(!user) return null;
+
+  console.log({user})
+
+
   return (
     <div>Sidebar</div>
   )
 }
 
 export default Sidebar
+
+
+const UnAuthenticatedSidebar = () => (
+  <div className="sticky top-20">
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-center text-xl font-semibold">Welcome Back!</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-center text-muted-foreground mb-4">
+          Login to access your profile and connect with others.
+        </p>
+        <SignInButton mode="modal">
+          <Button className="w-full" variant="outline">
+            Login
+          </Button>
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button className="w-full mt-2" variant="default">
+            Sign Up
+          </Button>
+        </SignUpButton>
+      </CardContent>
+    </Card>
+  </div>
+);
